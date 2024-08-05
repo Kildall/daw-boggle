fddc269cfeccf7013846ffafb597e7b3bdb44f16
Pedro Bustos <PedroPablo.Bustos@alumnos.uai.edu.ar>
2024-08-05 16:12:27 -0300

    ADD: agregada funcion de iniciar juego

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,32 @@
 "use strict";
 
 var distribucionLetras = {
-  A: 8.17,
-  B: 1.49,
-  C: 2.78,
-  D: 4.25,
-  E: 12.7,
-  F: 2.23,
-  G: 2.02,
-  H: 6.09,
-  I: 6.97,
-  J: 0.15,
-  K: 0.77,
-  L: 4.03,
-  M: 2.41,
-  N: 6.75,
-  O: 7.51,
-  P: 1.93,
-  Q: 0.1,
-  R: 5.99,
-  S: 6.33,
-  T: 9.06,
-  U: 2.76,
-  V: 0.98,
-  W: 2.36,
-  X: 0.15,
-  Y: 1.97,
-  Z: 0.07,
+    A: 8.17,
+    B: 1.49,
+    C: 2.78,
+    D: 4.25,
+    E: 12.7,
+    F: 2.23,
+    G: 2.02,
+    H: 6.09,
+    I: 6.97,
+    J: 0.15,
+    K: 0.77,
+    L: 4.03,
+    M: 2.41,
+    N: 6.75,
+    O: 7.51,
+    P: 1.93,
+    Q: 0.1,
+    R: 5.99,
+    S: 6.33,
+    T: 9.06,
+    U: 2.76,
+    V: 0.98,
+    W: 2.36,
+    X: 0.15,
+    Y: 1.97,
+    Z: 0.07,
 };
 
 // Constantes
@@ -63,8 +63,19 @@ var modalTitulo = document.getElementById("modal-titulo");
 var modalMensaje = document.getElementById("modal-mensaje");
 var btnCerrarModal = document.getElementById("modal-cerrar");
 var listaPalabrasIncorrectas = document.getElementById(
-  "lista-palabras-incorrectas"
+    "lista-palabras-incorrectas"
 );
 var btnMostrarModalRanking = document.getElementById("mostrar-ranking");
 var btnCerrarModalRanking = document.querySelector("#modal-ranking .cerrar");
 var btnOrdenarRanking = document.getElementById("orden-ranking");
+
+// Inicialización
+function iniciarJuego() {
+    formNombreJugador.addEventListener("submit", manejarInicioJuego);
+    btnEnviarPalabra.addEventListener("click", verificarPalabra);
+    btnNuevaPartida.addEventListener("click", reiniciarJuego);
+    btnCerrarModal.addEventListener("click", cerrarModal);
+    btnMostrarModalRanking.addEventListener("click", mostrarRanking);
+    btnCerrarModalRanking.addEventListener("click", cerrarModalRanking);
+    btnOrdenarRanking.addEventListener("change", actualizarTablaRanking);
+}
